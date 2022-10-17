30b287bf84041d636d871faa4172d3e4e8333d18
Erick Figueiredo <31293103+erickfigueiredo@users.noreply.github.com>
2022-10-17 17:17:19 -0300

    UPDATED - Contagens no Jogo de Azar

--- trab-1/badLuck.cpp
+++ trab-1/badLuck.cpp
@@ -1,15 +1,26 @@
 #include <iostream>
 #include <cstdlib>
+#include <list>
 #include <ctime>
 
 using namespace std;
 
 void simulate(long long n, int mA, int mB) {
     long long wins[] = { 0, 0 };
+
+    printf("Simulacao finalizada: %lld rodada(s)!\n", n);
+
     for (long long i = 0; i < n; i++) {
+        long long flipCoin = 0;
+        
+        printf("=> Round %lld:\n", i+1);
         int moneyA = mA;
         int moneyB = mB;
+
+        list<pair<int, int>> moneyPerRound;
+        moneyPerRound.push_back({moneyA, moneyB});
         do {
+            flipCoin++;
             int ran = rand();
             if (ran % 2) {
                 moneyA--;
@@ -20,13 +31,20 @@ void simulate(long long n, int mA, int mB) {
                 moneyB--;
             }
 
+            moneyPerRound.push_back({moneyA, moneyB});
         } while (moneyA && moneyB);
 
         if (!moneyA) wins[1]++;
         else wins[0]++;
+
+        for(const auto &p:moneyPerRound)
+            printf("%d,%d\n", p.first, p.second);
+
+        printf("-Numero de lancamentos: %lld\n\n", flipCoin);
     }
 
-    printf("Simulacao finalizada: %lld rodadas!\n - Vitorias de A: %.3f%%\n - Vitorias de B: %.3f%%\n\n", n, (wins[0] * 100.0) / n, (wins[1] * 100.0) / n);
+    printf("- Vitorias de A: %.3f%%\n- Vitorias de B: %.3f%%\n\n", (wins[0] * 100.0) / n, (wins[1] * 100.0) / n);
+
 }
 
 int main(int argc, char** argv) {
@@ -48,7 +66,7 @@ int main(int argc, char** argv) {
         cin >> simulations;
 
         simulate(simulations, monA, monB);
-    }   
+    }
 
     return 0;
 }
